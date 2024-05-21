5820c17eb825e0b3be336db3f883afe30ec04b43
Raulmora22 <raulprogrammerr@gmail.com>
2024-05-21 17:22:59 +0200

    fix main jsx

---
+++ src/components/Buttons/Button1.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Button1() {
+  return (
+    <div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default Button1;
---
+++ src/components/Buttons/Button2.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Button2() {
+  return (
+    <div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default Button2;
