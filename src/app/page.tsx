4547cef4e624deb1b7adb267e8e422cfaac9d637
Raulmora22 <raulprogrammerr@gmail.com>
2024-05-21 19:58:22 +0200

    change 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Button1 from "../components/Buttons/Button1";
 import Button2 from "../components/Buttons/Button2";
+import Card1 from "../components/Card/Card1.jsx";
 import React from "react";
 export default function Home() {
-  return <div className="items-center justify-center flex p-20"></div>;
+  return (
+    <div className="items-center justify-center flex p-20">
+      <Card1 />
+    </div>
+  );
 }
 
 /* 
---
+++ src/components/Card/Card1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card1() {
+  return (
+    <div className="">
+      <img src="" alt="---" />
+    </div>
+  );
+}
+
+export default Card1;
